722ba1be67b65d717ea516b2fd477d3becbed93d
williamgt <william.tresselt@gmail.com>
2022-09-11 13:41:50 +0200

    Can rotate cube, and fixed weird bug where bg color visible through front

--- oblig_3/main.cpp
+++ oblig_3/main.cpp
@@ -3,64 +3,66 @@
 #include <GL/glut.h>
 #include <iostream>
 
+int rotate_x = 0;
+int rotate_y = 0;
 int rotate_z = 0;
 
 void drawSingleCube(GLdouble x, GLdouble y, GLdouble z) {
+  glPushMatrix();
   // Translating cube
   glTranslated(x, y, z);
 
   // Drawing cube
   glBegin(GL_QUADS);
-  // Top face
-  glColor3d(1.0, 0, 0);
-  glVertex3f(1.0, 1.0, 1.0);
-  glVertex3f(1.0, 1.0, -1.0);
-  glVertex3f(-1.0, 1.0, -1.0);
-  glVertex3f(-1.0, 1.0, 1.0);
-
   // Front face
-  glColor3d(1.0, 1.0, 1.0);
+  glColor3d(1.0, 1.0, 0.0); // Yellow
   glVertex3f(1.0, 1.0, 1.0);
   glVertex3f(-1.0, 1.0, 1.0);
   glVertex3f(-1.0, -1.0, 1.0);
   glVertex3f(1.0, -1.0, 1.0);
-
-  // Right face
-  glColor3d(0.0, 0.0, 1.0);
-  glVertex3f(1.0, 1.0, -1.0);
-  glVertex3f(1.0, 1.0, 1.0);
-  glVertex3f(1.0, -1.0, 1.0);
-  glVertex3f(1.0, -1.0, -1.0);
 
   // Back face
-  glColor3d(1.0, 1.0, 0.0);
+  glColor3d(1.0, 1.0, 1.0); // White
   glVertex3f(1.0, 1.0, -1.0);
-  glVertex3f(-1.0, 1.0, -1.0);
-  glVertex3f(-1.0, -1.0, -1.0);
   glVertex3f(1.0, -1.0, -1.0);
+  glVertex3f(-1.0, -1.0, -1.0);
+  glVertex3f(-1.0, 1.0, -1.0);
 
   // Left face
-  glColor3d(0.0, 1.0, 0.0);
+  glColor3d(0.0, 1.0, 0.0); // Green
   glVertex3f(-1.0, 1.0, 1.0);
   glVertex3f(-1.0, 1.0, -1.0);
   glVertex3f(-1.0, -1.0, -1.0);
   glVertex3f(-1.0, -1.0, 1.0);
 
+  // Right face
+  glColor3d(0.0, 0.0, 1.0); // Blue
+  glVertex3f(1.0, 1.0, -1.0);
+  glVertex3f(1.0, 1.0, 1.0);
+  glVertex3f(1.0, -1.0, 1.0);
+  glVertex3f(1.0, -1.0, -1.0);
+
+  // Top face
+  glColor3d(1.0, 0.0, 0.0); // Red
+  glVertex3f(1.0, 1.0, 1.0);
+  glVertex3f(1.0, 1.0, -1.0);
+  glVertex3f(-1.0, 1.0, -1.0);
+  glVertex3f(-1.0, 1.0, 1.0);
+
   // Bottom face
-  glColor3d(1.0, 0.5, 0.0);
+  glColor3d(1.0, 0.5, 0.0); // Orange
   glVertex3f(1.0, -1.0, -1.0);
   glVertex3f(1.0, -1.0, 1.0);
   glVertex3f(-1.0, -1.0, 1.0);
   glVertex3f(-1.0, -1.0, -1.0);
   glEnd();
 
-  glTranslated(-x, -y, -z);
+  glPopMatrix();
 }
 
 void draw3x3Cubes() {
-  GLdouble gap = 0.1;
+  GLdouble gap = 0.02;
   int cube_size = 2;
-  int translate_back = -5;
 
   for (auto i = 1; i < 4; i++) {
     for (auto j = 1; j < 4; j++) {
@@ -68,19 +70,22 @@ void draw3x3Cubes() {
         drawSingleCube(
             (gap + cube_size) * (i - 2),
             (gap + cube_size) * (j - 2),
-            (gap + cube_size) * (k - 2 + translate_back));
+            (gap + cube_size) * (k - 2));
       }
     }
   }
 }
 
-void clear() {
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-}
-
 void display() {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
   glMatrixMode(GL_MODELVIEW);
   glLoadIdentity();
+  glEnable(GL_CULL_FACE);
+  glCullFace(GL_BACK);
+
+  glRotated(rotate_x, 1.0, 0.0, 0.0);
+  glRotated(rotate_y, 0.0, 1.0, 0.0);
   glRotated(rotate_z, 0.0, 0.0, 1.0);
 
   draw3x3Cubes();
@@ -88,16 +93,12 @@ void display() {
   glFlush();
 }
 
-void clearAndDisplay(void) {
-  clear();
-  display();
-}
-
 void init() {
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   // gluPerspective(60.0f, h/w, 1.0f, 50.0f);
   glFrustum(-2.0, 2.0, -2.0, 2.0, 1.0, 50.0);
+  gluLookAt(0.0, 0.0, 10.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
 
   // glMatrixMode(GL_MODELVIEW);
   // glLoadIdentity();
@@ -107,15 +108,6 @@ void init() {
 void reshape(int w, int h) {
   glViewport(0, 0, (GLsizei)w, (GLsizei)h);
 }
-
-// void reshape(int w, int h) {
-//   glViewport(0, 0, (GLsizei)w, (GLsizei)h);
-//   glMatrixMode(GL_PROJECTION);
-//   glLoadIdentity();
-//   glFrustum(-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
-//   glMatrixMode(GL_MODELVIEW);
-//   glLoadIdentity();
-// }
 
 void keybInput(unsigned char key, int x, int y) {
   switch (key) {
@@ -132,9 +124,17 @@ void keybInput(unsigned char key, int x, int y) {
 
 void specialKeybInput(int key, int x, int y) {
   switch (key) {
-  case GLUT_KEY_RIGHT:
+  case GLUT_KEY_END:
     rotate_z = (rotate_z + 1) % 360;
-    std::cout << "Hei" << rotate_z << std::endl;
+    glutPostRedisplay();
+    break;
+  case GLUT_KEY_RIGHT:
+    rotate_x = (rotate_x + 1) % 360;
+    glutPostRedisplay();
+    break;
+  case GLUT_KEY_UP:
+    rotate_y = (rotate_y + 1) % 360;
+    glutPostRedisplay();
     break;
   default:
     break;
@@ -150,7 +150,7 @@ int main(int argc, char **argv) {
 
   init();
   glutReshapeFunc(reshape);
-  glutDisplayFunc(clearAndDisplay);
+  glutDisplayFunc(display);
   //  glutKeyboardFunc(keybInput);
   glutSpecialFunc(specialKeybInput);
 
